0a220fbd02a284dbc03e5dc5f1317af14e8f9979
Nada Hussien <84433181+NadaHussien12@users.noreply.github.com>
2021-09-10 16:49:38 +0200

    Commit(Home Gate Automation System)

---
+++ Home_Gate_Automation_System1.ino
@@ -0,0 +1,30 @@
+//Declaration and initialization for some variables: 
+#define MOTOR1 8
+#define MOTOR2 9
+const int buzzer = 6;
+
+
+
+void setup()
+{
+  pinMode(buzzer,OUTPUT);
+  pinMode(MOTOR1,OUTPUT);
+  pinMode(MOTOR2,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop()
+{
+
+//Motor Code:
+  digitalWrite(MOTOR1,HIGH);
+  digitalWrite(MOTOR2,LOW);
+  
+
+//Buzzer Code:
+
+  // Send 1KHz sound signal for 1 sec.
+  tone(buzzer, 1000); 
+  delay(1000); 
+  
+}
